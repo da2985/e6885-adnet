db9d1d5b8fae904d616fa4d95989d9c197f9e258
kaanyarali1 <77569866+kaanyarali1@users.noreply.github.com>
2021-12-20 01:29:58 -0500

    Add files via upload

--- adnetRNN.py
+++ adnetRNN.py
@@ -1,14 +1,12 @@
-from tensorflow.keras import layers
 class ADNET_v3(tf.keras.Model):
 
     ACTION_DIM = 11
     K = 10
+    CONF_SCORE_DIM = 2
+
 
     def __init__(self):
         super(ADNET_v3, self).__init__()
-
-        #self.action_history = tf.zeros(shape = (1,1, ADNET_v3.ACTION_DIM * ADNET_v3.K))
-        self.action_history = tf.zeros(shape = (1,1, 512))
 
         self.conv1 = tf.keras.layers.Conv2D(filters = 96, kernel_size = (7, 7), strides = (2, 2), padding = 'VALID', activation = 'relu', name = 'conv_1')
         self.max1  = tf.keras.layers.MaxPooling2D(pool_size=(3,3), strides = (1, 1), padding = 'VALID')
@@ -20,14 +18,9 @@ class ADNET_v3(tf.keras.Model):
         self.fc1 = tf.keras.layers.Conv2D(filters = 512, kernel_size = (3, 3), padding = 'VALID', activation = 'relu', name = 'fc1')
         self.rnn = CustomRNN()
         
-
-    def build(self, action_history):
+    def build(self,action_history):
       super(ADNET_v3, self).build((None, 112, 112, 3))
-      self.setActionHistory(action_history)
-
-    def setActionHistory(self, action_history):
-      #self.action_history = self.rnn.setActionHistory(action_history.reshape((1, 1, 1, ADNET_v3.ACTION_DIM * ADNET_v3.K)))
-      self.action_history = self.rnn.setActionHistory(action_history.reshape((1, 1, 1, 512)))
+      self.rnn.setActionHistory(action_history)
 
     def call(self, input_tensor, training=False):
         x = self.conv1(input_tensor)
@@ -39,7 +32,6 @@ class ADNET_v3(tf.keras.Model):
 
         x = self.fc1(x)
         action,confidence = self.rnn(x)
-
         return [action, confidence]
 
     def callForAction(self, input_tensor, training=False):
@@ -49,8 +41,11 @@ class ADNET_v3(tf.keras.Model):
       '''
       return self.call(input_tensor, training)[0]
 
+    def clearActionHistory(self):
+      self.rnn.clearActionHistory()
+
     def compile(self, optimizer):
-   		super().compile(optimizer, loss={
+      super().compile(optimizer, loss={
          'output_1':tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
          'output_2': tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
          })
@@ -66,10 +61,9 @@ class ADNET_v3(tf.keras.Model):
 class CustomRNN(layers.Layer):
     def __init__(self):
         super(CustomRNN, self).__init__()
-        self.projection_1 = tf.keras.layers.Conv2D(filters = 512, kernel_size = (1, 1), padding = 'VALID')
-        self.projection_2 = tf.keras.layers.Conv2D(filters = 512, kernel_size = (1, 1), padding = 'VALID')
-        #self.action_history = tf.zeros(shape = (1,1,1, ADNET_v3.ACTION_DIM * ADNET_v3.K))
-        self.action_history = tf.zeros(shape = (1,1,1, 512))
+        self.projection_1 = tf.keras.layers.Conv2D(filters = 256, kernel_size = (1, 1), padding = 'VALID')
+        self.projection_2 = tf.keras.layers.Conv2D(filters = 256, kernel_size = (1, 1), padding = 'VALID')
+        self.action_history = tf.zeros(shape = (1,1,1,256))
         self.action_classifier = tf.keras.layers.Conv2D(filters = 11, kernel_size = (1,1), padding = 'VALID',activation="softmax")
         self.confidence_classifier = tf.keras.layers.Conv2D(filters = 2, kernel_size = (1,1), padding = 'VALID',trainable=False,activation="softmax")
 
@@ -77,10 +71,13 @@ class CustomRNN(layers.Layer):
         h = self.projection_1(self.action_history)
         y = h + self.projection_2(input)
         new_action_history = tf.math.tanh(y)
-        self.action_history = new_action_history
+        self.setActionHistory(new_action_history)
         action = self.action_classifier(y)
         confidence = self.confidence_classifier(y)
         return action,confidence
 
     def setActionHistory(self, action_history):
       self.action_history = action_history
+    
+    def clearActionHistory(self):
+      self.setActionHistory(tf.zeros((1, 1, 1, 256)))
